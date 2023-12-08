d6a1b77c755d631b1153238541fd43557751529b
senaiapy <senaia.py@gmail.com>
2023-12-08 12:58:55 -0300

    feat: binary

--- DOC/install-FULL.sh
+++ DOC/install-FULL.sh
@@ -5,3 +5,11 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 echo PATH=${PATH} > ~/.huskyrc
+
+# npx create-senaiapy-app@latest senaia
+# npx create-obytes-app@latest senaia
+
+pnpm i
+npx esbuild index.js  --bundle --outfile=build.cjs --format=cjs --platform=node
+npx pkg build.cjs
+./build-linux
